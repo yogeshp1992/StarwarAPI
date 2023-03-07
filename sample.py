7c84bcf8876a499abec56e359077afc7a8b23772
yogesh <yogesh30892@gmail.com>
2023-03-07 13:38:14 +0530

    new add

---
+++ sample.py
@@ -0,0 +1,21 @@
+
+from dataclasses import dataclass
+
+
+@dataclass
+class InventoryItem:
+    """Class for keeping track of an item in inventory."""
+    name: str
+    unit_price: float
+    quantity_on_hand: int = 0
+
+    def total_cost(self) -> float:
+        return self.unit_price * self.quantity_on_hand
+
+
+if __name__ == "__main__":
+
+    data = {"name": "laptop", "unit_price": "", "quantity_on_hand": 30}
+
+    validated_data = InventoryItem(**data)
+    breakpoint()
